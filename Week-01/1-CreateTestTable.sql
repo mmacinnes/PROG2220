53cb0a8cc930b6a5e9a2ff79bf045fee8b541f07
mmacinnes <mmacinnes@conestogac.on.ca>
2024-12-16 14:05:23 -0500

    Initial Load

---
+++ Week-01/1-CreateTestTable.sql
@@ -0,0 +1,4 @@
+Create Table test_table (
+    id INT PRIMARY KEY,
+    name VARCHAR(255)
+);
---
+++ Week-01/2-AddRowsTestTable.sql
@@ -0,0 +1,7 @@
+INSERT into test_table
+    (id, name)
+VALUES
+    (1, 'John'),
+    (2, 'Jane'),
+    (3, 'Joe'),
+    (4, 'Jill');
